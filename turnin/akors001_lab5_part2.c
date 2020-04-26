5a72e096053b590bc42d97d9f246b7dceac9f5e3
ArmaniKorsich <Armanikorsich@gmail.com>
2020-04-25 23:05:08 -0700

    completed part 2

---
+++ turnin/akors001_lab5_part2.c
@@ -0,0 +1,95 @@
+/*	Author: armanikorsich
+ *  Partner(s) Name: 
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+enum SM_States { SM_Start, inc, dec, incpause, decpause} state;
+
+void TickFct() {
+
+	switch(state) {
+		case SM_Start:
+			state = SM_Start;
+			if ((PINA & 0x01) == 0x00) {
+				state = inc;
+			} else if ((PINA & 0x02) == 0x00) {
+				state = dec;
+			}
+			break;
+		case incpause:
+			if ((PINA & 0x01) == 0x00) {
+				state = incpause;
+			} else {
+				state = SM_Start;
+			}
+			break;
+		case decpause:
+			if ((PINA & 0x02) == 0x00) {
+				state = decpause;
+			} else { //when they let it go
+				state = SM_Start;
+			}
+			break;
+		case inc:
+			state = incpause;
+			break;
+		case dec:
+			state = decpause;
+			break;
+		default:
+			state = SM_Start;	
+			break;
+
+	}
+
+	switch(state) {
+		case SM_Start:
+			if ((PINA & 0x03) == 0x00) {
+				PORTC = 0x00;
+			}
+			break;
+		case inc:
+			if (PORTC < 0x09) {
+				PORTC = PORTC + 1;
+			}
+			break;
+		case dec:
+			if (PORTC > 0x00) {
+				PORTC = PORTC - 1;
+			}
+	
+		case incpause:
+		case decpause:
+			if ((PINA & 0x03) == 0x00) {
+				PORTC = 0x00;
+			}
+			break;
+		default:
+			PORTC = 0x07;
+			break;
+	}
+	
+}
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+   state = SM_Start;
+   DDRA = 0x00; PORTA = 0xFF;
+   DDRC = 0xFF; PORTC = 0x00;
+
+   PORTC = 0x07;
+    /* Insert your solution below */
+    while (1) {
+	TickFct();
+    }
+    return 1;
+}
